03a5a008c16539139ecfff525ac150b8b0e55d35
18102038 <18102038@ittelkom-pwt.ac.id>
2021-06-23 22:10:25 +0700

    10.5.1 Update Data

---
+++ practice10/simpan_mahasiswa.php
@@ -0,0 +1,26 @@
+<?php 
+    include "koneksi.php";
+
+    if(isset($_POST['mahasiswa_id'])) {
+        //Kondisi Update
+        $sql = "UPDATE mahasiswa SET nama_lengkap='".$_POST['nama_lengkap']."',alamat='".$_POST['alamat']."',kelas_id='".$_POST['kelas_id']."'WHERE(`mahasiswa_id`='".$_POST['mahasiswa_id']."');";
+        if($conn->query($sql) === TRUE) {
+            $conn->close();
+            header("location:index.php");
+            exit();
+        }else{
+            $conn->close();
+            header("location:index.php");
+            exit();
+        }
+    }else{
+        $sql = "INSERT INTO mahasiswa (nama_lengkap, kelas_id, alamat) VALUES ('".$_POST['nama_lengkap']."', '".$_POST['kelas_id']."', '".$_POST['alamat']."')";
+        if($conn->query($sql) == TRUE) {
+            $conn->close();
+        }else {
+            echo "Error: ".$sql."<br>".$conn->error;
+        }
+        header("location:index.php");
+        exit();
+    }
+?>
