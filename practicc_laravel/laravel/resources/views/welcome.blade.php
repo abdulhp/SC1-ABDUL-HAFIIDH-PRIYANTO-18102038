fb1f4897a405a7848982c3610b6389e29c51f603
18102038 <18102038@ittelkom-pwt.ac.id>
2021-07-04 20:42:30 +0700

    11.6 Tugas

--- practicc_laravel/laravel/resources/views/welcome.blade.php
+++ practicc_laravel/laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Abdul Hafiidh Priyanto
                 </div>
 
                 <div class="links">
